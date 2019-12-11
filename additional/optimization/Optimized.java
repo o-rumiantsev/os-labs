4a0b3dfbdd367791515dfe9c43a5c27c63fd8e83
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2019-12-11 12:55:43 +0200

    Implement additional works

--- additional/optimization/Optimized.java
+++ additional/optimization/Optimized.java
@@ -1,25 +1,19 @@
 public class Optimized {
     public static void main(String[] args) {
-        int[][] a = new int[1000][1000];
+        int[][] a = new int[10000][1000];
         int res = 0;
 
-        long startTime = System.nanoTime();
-
-        for (int i = 999; i > 0; --i) {
+        for (int i = 9999; i > 0; --i) {
             for (int j = 999; j > 0; --j) {
                 a[i][j] = a[i - 1][j - 1] + 1;
             }
         }
 
-        for (int i = 0; i < 1000; ++i) {
+        for (int i = 0; i < 10000; ++i) {
             for (int j = 0; j < 1000; ++j) {
                 a[i][j]--;
             }
         }
-
-        long elapsedTime = System.nanoTime() - startTime;
-
-        System.out.println("Total execution time: " + elapsedTime);
 
     }
 }
